d8bc2b40eba3467d3db415b5fb20124cf3d80496
katriosa <ee.chistyakova@gmail.com>
2024-01-20 15:01:47 +0100

    feat: third task done

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require("path");
+const dirPath = path.join(__dirname, "secret-folder");
+  
+// Читаем католог
+fs.readdir(dirPath, { withFileTypes: true }, (err, files) => { 
+  if (err) 
+    console.log(err); 
+  else { 
+    files.forEach(file => { 
+      if (file.isFile()) {
+        const fileName = path.parse(file.name).name
+        const fileExtname = (path.extname(file.name)).slice(1);
+        const filePath = path.join(__dirname, "secret-folder", file.name);
+ 
+        //определяем размер файлов
+        fs.stat(filePath, (err, stats) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          const fileSize = `${stats.size} bytes`
+          console.log(`${fileName} - ${fileExtname} - ${fileSize}`);
+        });
+      }
+    }) 
+  } 
+}) 
